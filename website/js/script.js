b8f5e1c33ac313776bb6ffbde7508dd8a04ef7ab
Mads Møller Laursen <eaamml@students.eaaa.dk>
2019-12-13 10:23:07 +0100

    Final push

--- website/js/script.js
+++ website/js/script.js
@@ -135,6 +135,33 @@ function galleriPage() {
   burger.classList.remove("closed");
 }
 
+function omPage() {
+  let element = document.getElementById("menu");
+  let content = document.getElementById("heroimg");
+  let frontpage = document.getElementById("frontpage-div");
+  let menupage = document.getElementById("menupage-div");
+  let bookingpage = document.getElementById("bookingpage-div");
+  let galleripage = document.getElementById("galleripage-div");
+  let ompage = document.getElementById("ompage-div");
+  let burger = document.getElementById("nav-icon1");
+  element.classList.remove("menu-visible");
+  element.classList.add("menu-closed");
+  frontpage.classList.add("front-closed");
+  frontpage.classList.remove("front");
+  content.classList.remove("heroimg-bgg-alt");
+  content.classList.add("heroimg-bgg");
+  menupage.classList.add("menupage-closed");
+  menupage.classList.remove("menupage-open");
+  bookingpage.classList.add("bookingpage-closed");
+  bookingpage.classList.remove("bookingpage-open");
+  galleripage.classList.add("galleripag-closed");
+  galleripage.classList.remove("galleripag-open");
+  ompage.classList.add("ompage-open");
+  ompage.classList.remove("ompage-closed");
+  burger.classList.remove("open");
+  burger.classList.remove("closed");
+}
+
 $(document).ready(function(){
   let menu = $("#menu");
   let heroimg = $("#heroimg");
